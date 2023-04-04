a3667f81403532a0711d5bd135d83fa9e0f7e516
iremerdonmez <i.erdonmez@emakina.com.tr>
2023-04-04 13:27:13 +0300

    add hw

---
+++ HW2/hw2.py
@@ -0,0 +1,28 @@
+"""
+Given an array of size n, find the most common and the least common elements.
+The most common element is the element that appears more than n // 2 times.
+The least common element is the element that appears fewer than other.
+
+You may assume that the array is non-empty and the most common element
+always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3, 2
+
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2, 1
+
+"""
+from typing import List, Tuple
+
+
+def major_and_minor_elem(inp: List) -> Tuple[int, int]:
+    most_common_list = max(set(inp), key= inp.count)
+    least_common_list = min(set(inp), key= inp.count)
+    print((most_common_list, least_common_list))
+    return((most_common_list, least_common_list))
+
